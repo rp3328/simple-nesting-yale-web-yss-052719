a5e63ab0bc9e046425731ee7115baf7174df275b
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-22 01:06:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -96,7 +96,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          :known_for => alans_new_info,
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
@@ -106,7 +106,6 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-programmer_hash[:alan_kay] && programmer_hash[:alan_kay][:known_for] = alans_new_info
 
 end
 
