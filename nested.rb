2271f0bcc4cc1dc39172f78155f19aaaabe91931
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-22 01:07:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -96,7 +96,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => alans_new_info,
+          :known_for => "GUI",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
